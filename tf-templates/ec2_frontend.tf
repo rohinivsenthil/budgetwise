5fbb95609398dd75bfc9d28f522aa7ae3ab635ba
Rohini <rv8542@rit.edu>
2024-03-27 23:13:47 -0400

    feat: add tf template to host fe (#12)
    * add tf template
    
    * update dockerfile

---
+++ tf-templates/ec2_frontend.tf
@@ -0,0 +1,64 @@
+resource "aws_instance" "budgetwise_instance" {
+  ami                    = "ami-09a6704a52d96773b"
+  instance_type          = "t4g.nano"
+  key_name               = "aws_key_0123"
+  vpc_security_group_ids = [aws_security_group.security_group_react.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # Update package index and install Docker
+    sudo yum update -y
+    sudo yum install -y docker
+    
+    # Start the Docker service
+    sudo service docker start
+    
+    # Start your Docker container
+    sudo docker run -d -p 80:80 rv8542/budgetwise-frontend
+    EOF
+
+  tags = {
+    Name = "budgetwise-app"
+  }
+}
+
+resource "aws_security_group" "security_group_react" {
+  name        = "react1-sg"
+  description = "allow http port"
+
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "react1-sg"
+  }
+}
