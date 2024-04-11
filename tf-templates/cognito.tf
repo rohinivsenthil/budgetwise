01df36ef19139bc12600a2d3e98e58cf944551a1
Rohini <rv8542@rit.edu>
2024-04-11 12:14:41 -0400

    feat: add tf files for cognito (#29)

---
+++ tf-templates/cognito.tf
@@ -0,0 +1,98 @@
+resource "aws_cognito_user_pool" "main" {
+  name = "my_user_pool"
+  username_attributes = ["email"]
+
+  schema {
+    attribute_data_type = "String"
+    name                = "email"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 2048
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "name"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 2048
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "phone_number"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 0
+      max_length = 2048
+    }
+  }
+
+  auto_verified_attributes   = ["email"]
+  email_verification_message = "Click the link below to verify your email address: {####}"
+  email_verification_subject = "Verify Your Email"
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                                 = "app_client"
+  user_pool_id                         = aws_cognito_user_pool.main.id
+  generate_secret                      = false
+  explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH"]
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["phone", "email", "openid", "profile"]
+  allowed_oauth_flows_user_pool_client = true
+  callback_urls                        = ["https://www.example.com/callback"]
+  logout_urls                          = ["https://www.example.com/logout"]
+}
+
+resource "aws_cognito_identity_pool" "main_identity_pool" {
+  identity_pool_name               = "my_identity_pool"
+  allow_unauthenticated_identities = false
+
+  cognito_identity_providers {
+    client_id               = aws_cognito_user_pool_client.client.id
+    provider_name           = aws_cognito_user_pool.main.endpoint
+    server_side_token_check = false
+  }
+}
+
+resource "aws_iam_role" "authenticated_role" {
+  name = "Cognito_Default_Authenticated_Role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRoleWithWebIdentity"
+      Effect    = "Allow"
+      Principal = {
+        Federated = "cognito-identity.amazonaws.com"
+      }
+      Condition = {
+        StringEquals = {
+          "cognito-identity.amazonaws.com:aud" = aws_cognito_identity_pool.main_identity_pool.id
+        },
+        "ForAnyValue:StringLike": {
+          "cognito-identity.amazonaws.com:amr" = "authenticated"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_cognito_identity_pool_roles_attachment" "main_identity_pool_role_attachment" {
+  identity_pool_id = aws_cognito_identity_pool.main_identity_pool.id
+
+  roles = {
+    "authenticated" = aws_iam_role.authenticated_role.arn
+  }
+}
--- tf-templates/output.tf
+++ tf-templates/output.tf
@@ -9,3 +9,21 @@ output "instance_public_ip" {
 output "sns_topic_arn" {
   value = aws_sns_topic.alert.arn
 }
+
+output "cognito_config" {
+  value = {
+    aws_project_region                   = "us-east-1"
+    aws_user_pools_id                    = aws_cognito_user_pool.main.id
+    aws_user_pools_web_client_id         = aws_cognito_user_pool_client.client.id
+    aws_cognito_username_attributes      = ["EMAIL"]
+    aws_cognito_signup_attributes        = ["EMAIL", "NAME", "PHONE_NUMBER"]
+    aws_cognito_mfa_configuration        = "OFF"
+    aws_cognito_mfa_types                = ["SMS"]
+    aws_cognito_password_protection_settings = {
+      passwordPolicyMinLength           = 8
+      passwordPolicyCharacters          = []
+    }
+    aws_cognito_verification_mechanisms = ["EMAIL"]
+  }
+  description = "Configuration for the AWS Cognito resources"
+}
